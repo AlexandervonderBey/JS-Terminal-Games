2d59cb7a874ab5d0a39d801b24d8e7dce2ccd3fa
Alexander  von der Bey <alexandervonderbey@gmail.com>
2025-01-14 09:43:40 +0100

    adding empty Ceasar Cypher js file

---
+++ Ceasar-Cypher.js
@@ -0,0 +1,47 @@
+/* 
+Caesar Cipher
+Description: Implement a basic Caesar Cipher encryption.
+Requirements:
+The program should take a phrase and a shift number as inputs from process.argv.
+Encrypt the phrase by shifting each letter, based on its position in the english alphabet, by the specified number.
+Case insensitive
+A negative shift means shift to the left
+A positive shift means shift to the right
+Output the encrypted phrase to the console.
+Example:
+node caesarCipher.js "hello world" 3
+# Output: khoor zruog
+*/
+
+/*
+a = 1
+b = 2
+c = 3
+d = 4
+e = 5
+f = 6
+g = 7
+h = 8
+i = 9
+j = 10
+k = 11
+l = 12
+m = 13
+n = 14
+o = 15
+p = 16
+q = 17
+r = 18
+s = 19
+t = 20
+u = 21
+v = 22
+w = 23
+x = 24
+y = 25
+z = 26
+*/
+
+
+//#################################################################################################
+
